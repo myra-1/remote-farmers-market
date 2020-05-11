80f211857d2700cb2a35e77ba084885b696db486
Myra <myrakornides@gmail.com>
2020-05-11 14:07:36 -0400

    rails user auth and cors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :display_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :password, length: {minimum: 4}
+
 end
+
+
+def return_data 
+  {
+    id: id,
+    display_name: display_name,
+    email: email,
+    created_at: created_at,
+    updated_at: updated_at
+  }

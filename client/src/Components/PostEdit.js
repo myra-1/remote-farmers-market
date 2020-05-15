ced0a74171e17fe1eff9f4bdff92c1186589fcbd
Myra <myrakornides@gmail.com>
2020-05-15 10:25:53 -0400

    edits to postedit

--- client/src/Components/PostEdit.js
+++ client/src/Components/PostEdit.js
@@ -8,13 +8,15 @@ class PostEdit extends Component {
     super(props)
 
     this.state = {
-      id: '',
-      title: '',
-      description: '',
-      img_url: '',
-      price: '',
-      quantity: '',
-      contact_info: '',
+      postInfo: {
+        id: '',
+        title: '',
+        description: '',
+        img_url: '',
+        price: '',
+        quantity: '',
+        contact_info: '',
+      }
     }
   }
 
@@ -22,22 +24,31 @@ class PostEdit extends Component {
     let { id } = this.props.match.params;
     const postInfo = await getOnePost(id);
     this.setState({
-      id: postInfo.id,
-      title: postInfo.title,
-      description: postInfo.description,
-      img_url: postInfo.img_url,
-      price: postInfo.price,
-      quantity: postInfo.quantity,
-      contact_info: postInfo.contact_info,
+      postInfo
+      // id: postInfo.id,
+      // title: postInfo.title,
+      // description: postInfo.description,
+      // img_url: postInfo.img_url,
+      // price: postInfo.price,
+      // quantity: postInfo.quantity,
+      // contact_info: postInfo.contact_info,
     })
   }
 
   handlePostUpdate = async (event) => {
-    event.preventDefault()
+    // event.preventDefault()
     let { id } = this.state.id
     let postInfo = await getOnePost(id)
     const editPost = await updatePost(id, postInfo)
     this.setState({ editPost })
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target
+    this.setState({
+      ...this.state,
+      // [name]: value
+    })
   }
 
   handlePostDelete = async (id) => {
@@ -45,12 +56,6 @@ class PostEdit extends Component {
     this.props.history.push('/posts');
   }
 
-  handleChange = (event) => {
-    const { value } = event.target
-    this.setState({
-      name: value
-    })
-  }
 
   render() {
     return (
@@ -65,38 +70,38 @@ class PostEdit extends Component {
           <input
             type="text"
             name="title"
-            value={this.state.title}
+            value={this.state.postInfo.title}
             onChange={this.handleChange}
           />
           <label htmlFor="description">Description</label>
           <input
             type="text"
             name="description"
-            value={this.state.description}
+            value={this.state.postInfo.description}
             onChange={this.handleChange}
           />
           <label htmlFor="price">Price</label>
           <input
             type="text"
             name="price"
-            value={this.state.price}
+            value={this.state.postInfo.price}
             onChange={this.handleChange}
           />
           <label htmlFor="quantity">Quantity</label>
           <input
             type="text"
             name="quantity"
-            value={this.state.quantity}
+            value={this.state.postInfo.quantity}
             onChange={this.handleChange}
           />
           <label htmlFor="contact_info">Contact:</label>
           <input
             type="text"
             name="contact_info"
-            value={this.state.contact_info}
+            value={this.state.postInfo.contact_info}
             onChange={this.handleChange}
           />
-          <button type='submit'>Save</button>
+          <button onClick={() => { this.handlePostUpdate(this.state.id) }}>Save</button>
         </form>
 
         <button onClick={() => { this.handlePostDelete(this.state.id) }}>Delete</button>
